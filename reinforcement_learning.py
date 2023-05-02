54f021e5fa5e810cd5d3425889fd6c219f69a40f
maximpopov11 <icecreamguy0@gmail.com>
2023-05-02 11:43:40 -0500

    Addressed warnings in DQNAgent, expect bugs.

--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -6,14 +6,13 @@ from collections import deque
 
 
 class DQNAgent:
-    def __init__(self, state_space_size, action_space_size, learning_rate=0.001, discount_factor=0.99,
+    def __init__(self, env, state_space_size, action_space_size, learning_rate=0.001, discount_factor=0.99,
                  batch_size=64, memory_size=100000, exploration_rate=1.0, exploration_decay_rate=0.99995):
+        self.env = env
         self.state_space_size = state_space_size
         self.action_space_size = action_space_size
-        self.learning_rate = learning_rate
         self.discount_factor = discount_factor
         self.batch_size = batch_size
-        self.memory_size = memory_size
         self.exploration_rate = exploration_rate
         self.exploration_decay_rate = exploration_decay_rate
         self.memory = deque(maxlen=memory_size)
@@ -43,8 +42,35 @@ class DQNAgent:
                 q_values = self.q_network(state)
                 return q_values.argmax().item()
 
-    def train(self, num_episodes=1000, max_steps_per_episode=1000, batch_size=64, discount_factor=0.99,
-              target_update_frequency=10, epsilon_decay=0.99):
+    def learn(self, batch_size, discount_factor):
+        minibatch = random.sample(self.memory, batch_size)
+        states_batch, action_batch, reward_batch, next_states_batch, done_batch = zip(*minibatch)
+
+        states_batch = torch.tensor(states_batch).float().to(self.device)
+        action_batch = torch.tensor(action_batch).to(self.device)
+        reward_batch = torch.tensor(reward_batch).float().to(self.device)
+        next_states_batch = torch.tensor(next_states_batch).float().to(self.device)
+        done_batch = torch.tensor(done_batch).to(self.device)
+
+        # Compute Q values for current states
+        q_values = self.q_network(states_batch)
+        q_values = q_values.gather(1, action_batch.unsqueeze(1)).squeeze(1)
+
+        # Compute target Q values for next states
+        target_q_values = self.q_network(next_states_batch)
+        max_target_q_values = target_q_values.max(1)[0]
+        target_q_values = reward_batch + (1 - done_batch) * discount_factor * max_target_q_values
+
+        # Compute loss and update Q network
+        loss = nn.MSELoss()(q_values, target_q_values.detach())
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+    def update_target_network(self):
+        self.q_network.load_state_dict(self.q_network.state_dict())
+
+    def train(self, num_episodes=1000, max_steps_per_episode=1000, target_update_frequency=10):
         # Loop over episodes
         for i in range(num_episodes):
             # Reset environment and get initial state
@@ -61,8 +87,8 @@ class DQNAgent:
                 # Remember the transition
                 self.remember(state, action, reward, next_state, done)
                 # Update the DQN agent
-                if len(self.memory) > batch_size:
-                    self.learn(batch_size, discount_factor)
+                if len(self.memory) > self.batch_size:
+                    self.learn(self.batch_size, self.discount_factor)
                 # Update the total reward and state
                 total_reward += reward
                 state = next_state
@@ -71,7 +97,7 @@ class DQNAgent:
                 if step % target_update_frequency == 0:
                     self.update_target_network()
                 # Decay epsilon
-                self.epsilon *= epsilon_decay
+                self.exploration_rate *= self.exploration_decay_rate
             # Print the episode's results
             print("Episode {}: Total Reward = {}, Steps = {}".format(i + 1, total_reward, step))
 
